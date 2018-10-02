47b3c5642b0696968112388e766140a4268a82e7
fourth pete <thefourthpete@gmail.com>
2018-10-02 19:24:20 +0200

    intial spam

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // initialization
-var socket = new WebSocket("wss://node2.wsninja.io"), space;
+var socket = new WebSocket("wss://node2.wsninja.io"), space, me, qs = document.querySelector.bind(document), players;
 
 socket.onopen = event => {
     // Connection opened, send client GUID to autenticate with wsninja server.
@@ -10,14 +10,15 @@ socket.onopen = event => {
 socket.addEventListener('message', function(event) {
     var message = JSON.parse(event.data);
     if (message.accepted === true) {
-      //TODO some join code
+        players = [];
     } else {
         HandleRecieve(message);
     }
 });
 
 window.onload = loadEvent => {
-  space = document.querySelector('.game');
+  space = qs('.game');
+  qs('#join-btn').onclick = Join;
 }
 
 //
@@ -28,4 +29,30 @@ function HandleRecieve(data) {
 // wrap for sending
 function Send(obj) {
   socket.send(JSON.stringify(obj));
+}
+
+function Join(e) {
+  Send({join: qs('#namein').value});
+  qs('.pop').style.display = 'none';
+  document.documentElement.webkitRequestFullscreen();
+}
+
+function Update() {
+
+}
+
+class Player {
+  contructor(name, id) {
+    this.name = name;
+    this.pos = [0,0];
+    this.El = document.createElement('DIV');
+    this.El.innerHTML = name;
+    this.El.setAttribute('id', id);
+    this.El.setAttribute('class', player);
+    space.append(this.El);
+  }
+
+  Update(vec) {
+
+  }
 }
